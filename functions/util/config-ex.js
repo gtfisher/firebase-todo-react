bb73522b040bea14b9b613529dc1cab3adad5503
grant tf <grant@tarrant-fisher.co.uk>
2021-01-18 07:38:22 +0000

    tidy up config

---
+++ functions/util/config-ex.js
@@ -0,0 +1,12 @@
+// config.js
+
+module.exports = {
+    apiKey: "AIzaSyDzPGxo4pUpQwo9FHOSJ_1WIidjfLnqmcw",
+    authDomain: "todoreactapp-957d2.firebaseapp.com",
+    databaseURL: "https://todoreactapp-957d2-default-rtdb.firebaseio.com",
+    projectId: "todoreactapp-957d2",
+    storageBucket: "todoreactapp-957d2.appspot.com",
+    messagingSenderId: "662454588566",
+    appId: "1:662454588566:web:e645a32104985763399eb7",
+    measurementId: "G-QX4BCQY2ZX"
+};
